f68a8f491794d38763c5f615344697e42147f748
unknown <maciej@contact.apurer.io>
2023-10-29 03:17:26 +0100

    projekt markus

--- types/openai.ts
+++ types/openai.ts
@@ -8,38 +8,24 @@ export interface OpenAIModel {
 }
 
 export enum OpenAIModelID {
-  GPT_3_5 = 'gpt-3.5-turbo',
-  GPT_3_5_AZ = 'gpt-35-turbo',
-  GPT_4 = 'gpt-4',
-  GPT_4_32K = 'gpt-4-32k',
+  Luna_LLAMA = 'luna-llama2',
+  LLAMA_2_13B = 'llama2-13b',
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
-export const fallbackModelID = OpenAIModelID.GPT_3_5;
+export const fallbackModelID = OpenAIModelID.Luna_LLAMA;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
-  [OpenAIModelID.GPT_3_5]: {
-    id: OpenAIModelID.GPT_3_5,
-    name: 'GPT-3.5',
+  [OpenAIModelID.Luna_LLAMA]: {
+    id: OpenAIModelID.Luna_LLAMA,
+    name: 'Luna ai llama 2',
+    maxLength: 12000,
+    tokenLimit: 4096,
+  },
+  [OpenAIModelID.LLAMA_2_13B]: {
+    id: OpenAIModelID.LLAMA_2_13B,
+    name: 'llama 2 13B',
     maxLength: 12000,
     tokenLimit: 4000,
-  },
-  [OpenAIModelID.GPT_3_5_AZ]: {
-    id: OpenAIModelID.GPT_3_5_AZ,
-    name: 'GPT-3.5',
-    maxLength: 12000,
-    tokenLimit: 4000,
-  },
-  [OpenAIModelID.GPT_4]: {
-    id: OpenAIModelID.GPT_4,
-    name: 'GPT-4',
-    maxLength: 24000,
-    tokenLimit: 8000,
-  },
-  [OpenAIModelID.GPT_4_32K]: {
-    id: OpenAIModelID.GPT_4_32K,
-    name: 'GPT-4-32K',
-    maxLength: 96000,
-    tokenLimit: 32000,
   },
 };
